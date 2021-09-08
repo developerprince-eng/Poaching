2e189216c5f17e1f3d9cf8212612f004869b7ec4
DeveloperPrince <prince.maposa@ciibo.com>
2021-09-08 22:56:13 +0200

    Clean Up

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
+
+
 def main():
-    df = pd.read_excel('RhinoPoaching.xlsx', sheet_name='Sheet2', header=1)  # can also index sheet by name or fetch all sheets
-    xlist = df['X'].tolist()
-    ylist = df['Y'].tolist()
-    zlist = df['Z'].tolist()
+    df = pd.read_excel("RhinoPoaching.xlsx", sheet_name='Sheet2',
+                       header=1)  # can also index sheet by name or fetch all sheets
+    x_list = df['X'].tolist()
+    y_list = df['Y'].tolist()
+    z_list = df['Z'].tolist()
 
-    print("Country X :: ",format(xlist))
-    print("Country Y :: ",format(ylist))
-    print("Country Z :: ",format(zlist))
+    print("Country X :: ", format(x_list))
+    print("Country Y :: ", format(y_list))
+    print("Country Z :: ", format(z_list))
 
-    np.array([xlist, ylist, zlist])
+    np.array([x_list, y_list, z_list])
 
 
 if __name__ == '__main__':
